cdff62b050e6cd5dd54b9e5dfa4be3139e53ffb4
Maryam-Aslam <maryamaslamspace@gmail.com>
2025-04-11 12:22:39 +0500

    logics task using if else

---
+++ mypractice.js
@@ -0,0 +1,223 @@
+// let input=(" enter name and password")
+// let values= input.split("")
+// let naam= values[0];
+// let password= values[1];
+
+// if(naam==="ali" && password==="0987"){
+//     document.write("loggedin succesfully")
+// }
+//  else if(naam !=="ali" ){
+//     document.write("wrong naam")
+//  }
+//  else if(password !=="0987" ){
+//     document.write("wrong naam")
+// }
+// else{
+// document.write("login failed")
+// }
+
+//-----------------------
+// //find averge of three
+// let input=("write only 3 digits")
+// if(input.length === 3 && !isNaN(input)){
+//     let num1=Number[input[0]];
+//     let num2=Number[input[1]];
+//     let num3=Number[input[2]];
+
+// let averge= (num1 + num2+ num3) /3;
+// alert('the averge is' + averge);
+// }
+// else {
+//     alert("write only 3 digit");}
+
+// // find largest number
+// let input=prompt("write only 3 digits")
+// if(input.length === 3 && !isNaN(input)){
+//     let num1=Number(input[0]);
+//     let num2=Number(input[1]);
+//     let num3=Number(input[2]);
+
+// let largest= Math.max(num1 , num2, num3);
+// alert( largest);
+// }
+// else {
+//     alert("write only 3 digit");}
+
+// find smallest number
+// let input=prompt("write only 3 digits")
+// if(input.length === 3 && !isNaN(input)){
+//     let num1=Number(input[0]);
+//     let num2=Number(input[1]);
+//     let num3=Number(input[2]);
+
+// let largest= Math.min(num1 , num2, num3);
+// alert( largest);
+// }
+// else {
+//     alert("write only 3 digit");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input=prompt("write only three digit to find avrge")
+// if(input.length==="3" && !isNaN(input)){
+//     let num1=Number(input[0]);
+//     let num2=Number(input[1]);
+//     let num3=Number(input[2]);
+//     let avarge=(num1 + num2+ num3) /3;
+//     alert(averge);
+
+// } else{
+//     alert("wrte only three digit")
+// }
+
+
+//find largest number
+// let input= prompt("write 3 number")
+// if(input.length===3 && isNaN(input)){
+//     num1=Number(input[0]);
+//     num2=Number(input[1]);
+//     num3=Number(input[2]);
+// let largest=Math.max(num1 , num2 , num3);
+// alert(largest);
+
+// }else{
+//     alert("write only three digit");
+// }
+
+
+// let input= prompt("write your name and password")
+// let value=input.split(" ");
+// let username=input[0];
+// let password=input[1];
+// if(usernmae==="admin" && password==="12345"){
+//     alert("login successfully");
+// } else if(usernam!=="admin")
+// alert("incorrect usernmae");
+// {
+//     else if (password !=="12345")
+//         alert("incorrect password");
+// }
+// else {
+//     alert("wrong credetials")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input= prompt("enter username and passwor");
+// let values=input.split(" ");
+// let username= values[0];
+// let password= values[1];
+// if(username==="admin" && password==="12345"){
+//     alert("login successfully")
+// }
+// else if (username !=="admin"){
+//     alert("incorrect username")
+// }
+//    else if (username !== "12345"){
+//         alert("incorrect password")}
+
+//     else {
+//         alert("invalid credentials")
+//     }
+
+
+
+
+
+
+// let input= prompt("find the averge of three digit");
+// if(input.length===3 && !isNaN(input)){
+//     let num1= Number(input[0]);
+//     let num2= Number(input[1]);
+//     let num3= Number(input[2]);
+//      let avarge= (num1+num2+num3)/3;
+//      alert("the averge is"+ avarge)
+// }else
+// {
+//     alert("write only three digits")
+// }
+
+
+// let input= prompt("find the largest of three digit");
+// if(input.length===3 && !isNaN(input)){
+//     let num1= Number(input[0]);
+//     let num2= Number(input[1]);
+//     let num3= Number(input[2]);
+//      let largest=Math.max (num1,num2,num3);
+//      alert("the largest is "+ largest)
+// }else
+// {
+//     alert("write only three digits")
+// }
+
+let day= Number(prompt("enter any number to get the day info"));
+switch(day){
+case 1:
+    document.write("its monday");
+    break;
+    case 2:
+        document.write("its tuesday");
+        break;
+        case 3:
+    document.write("its wednesday");
+    break;
+    case 4:
+        document.write("its thursday");
+        break;
+        case 5:
+    document.write("friday");
+    break;
+    case 6:
+        document.write("its saturday");
+        break;
+        case 7:
+        document.write("sundayyyy fundayyyy");
+        break;
+        default:
+            document.write("its not a week dayy");
+}
+let date= new Date().getDay();
+document.write(date);
--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-//USERNAME AND PASSWORD VERIFICATION
+// //USERNAME AND PASSWORD VERIFICATION
 // let username = prompt("enter a username")
 // let password = prompt("enter password")
 // if(username==="admin"  && password==="112233"){
@@ -8,18 +8,63 @@
 //     document.write("invalid credentials")
 // }
 
-//----------------
-//shows in one alert in one input ..
-let input= prompt("enter a username and password here ")
-let values = input.split(" ")
-let user= values [0];
-let pass= values [1];
-if (user==="maryam" && pass==="12345"){
-    alert("login succesfull")
-}else if(user !=="maryam"){
-    alert("incorect username")
-}else if(pass !=="12345"){
-    alert("incorect password")
-}else{
-    alert("login failed")
+// //----------------
+// //shows in one alert in one input ..
+// let input= prompt("enter a username and password here ")
+// let values = input.split(" ")
+// let user= values [0];
+// let pass= values [1];
+// if (user==="maryam" && pass==="12345"){
+//     alert("login succesfull")
+// }else if(user !=="maryam"){
+//     alert("incorect username")
+// }else if(pass !==12345){
+//     alert("incorect password")
+// }else{
+//     alert("login failed")
+// }
+
+
+// //find averge of three
+// let input = prompt("write only 3 digits")
+// if(input.length === 3 && !isNaN(input)){
+//     let num1=Number(input[0]);
+//     let num2=Number(input[1]);
+//     let num3=Number(input[2]);
+
+// let averge= (num1 + num2+ num3) /3;
+// alert('the averge is' + averge);
+// }
+// else {
+//     alert("write only 3 digit");}
+
+
+
+
+ // find slargest number
+let input=prompt("write only 3 digits")
+if(input.length === 3 && !isNaN(input)){
+    let num1=Number(input[0]);
+    let num2=Number(input[1]);
+    let num3=Number(input[2]);
+
+let largest= Math.max(num1 , num2, num3);
+alert("the largest number is" + largest);
 }
+else {
+    alert("write only 3 digit");}
+    
+
+
+// // find smallest number
+// let input=prompt("write only 3 digits")
+// if(input.length === 3 && !isNaN(input)){
+//     let num1=Number(input[0]);
+//     let num2=Number(input[1]);
+//     let num3=Number(input[2]);
+
+// let largest= Math.min(num1 , num2, num3);
+// alert( largest);
+// }
+// else {
+//     alert("write only 3 digit");}
